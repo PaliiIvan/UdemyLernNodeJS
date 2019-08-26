4a8d4617b6412a27f8f2dc1a3b8c596f991eabec
Ivan Palii <bloodshok96@gmail.com>
2019-08-26 11:25:26 +0300

    Clear project data

--- NodeBasic/src/index.ts
+++ NodeBasic/src/index.ts
@@ -3,17 +3,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 
-import adminRoute from './routes/admin';
-import shopRoute from './routes/shop';
-
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-
-
-app.use(adminRoute);
-app.use(shopRoute);
-
 
 app.use((req,res,next) => {
     res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
