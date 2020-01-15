ce305d7b2d22190df768a51939dd52b717866111
Ivan Palii <bloodshok96@gmail.com>
2020-01-15 17:35:36 +0200

    Create first Rest request

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const feedRoutes = require('./routes/feed.routes');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authoriazation');
+    next();
+})
+app.use('/feed', feedRoutes);
+
+app.listen(8080);
