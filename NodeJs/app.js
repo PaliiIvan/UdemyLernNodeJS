045127e3f77d062fd7daa854ab8a2c555203356f
Ivan Palii <bloodshok96@gmail.com>
2020-01-17 18:03:32 +0200

    Finishing rest auth

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
-
+const authRoutes = require('./routes/auth');
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -48,6 +48,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
